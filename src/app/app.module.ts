8a6e842e91ddeadc6b1a40016a662928eb340554
Andrew Greenberg <andrew@Andrews-MacBook-Air.local>
2017-02-26 21:39:36 -0500

    otheradded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Http } from '@angular/http';
 import { LoginComponent } from './components/login.component';
 
 import { CurrentDateComponent } from './components/currentDate.component';
@@ -14,8 +15,17 @@ import { routing } from './components/app.routing';
 
 import { FormsModule } from '@angular/forms';
 
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+export const firebaseConfig = {
+    apiKey: "AIzaSyCx-oL-c4MZyfZpRu6ay-SFiUPpLv7JBlc",
+    authDomain: "checkinapp-dc892.firebaseapp.com",
+    databaseURL: "https://checkinapp-dc892.firebaseio.com",
+    storageBucket: "checkinapp-dc892.appspot.com",
+    messagingSenderId: "761473046167"
+}
+
 @NgModule({
-  imports:      [ BrowserModule, routing, FormsModule ],
+  imports:      [ BrowserModule, routing, FormsModule, Http, AngularFireModule.initializeApp(firebaseConfig) ],
   declarations: [ AppComponent, LoginComponent, CurrentDateComponent,
                   CurrentTimeComponent, CheckinComponent, CheckoutComponent,
                   
--- src/app/components/login.component.ts
+++ src/app/components/login.component.ts
@@ -4,11 +4,17 @@ import { Component,
         style,
         transition,
         animate,
-        keyframes
+        keyframes,
+        OnInit,
+        HostBinding
 
  } from '@angular/core';
 import { CurrentDateComponent } from './currentDate.component';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
+//import { moveIn } from '';
+
 
 @Component({
   moduleId: module.id,
@@ -23,8 +29,41 @@ import { FormsModule } from '@angular/forms';
       
   ]
 }) 
-export class LoginComponent  {
+export class LoginComponent implements OnInit {
+  error: any;
+
   state: string = "slideIn";
+
+  constructor(public af: AngularFire, private router: Router ){
+    this.af.auth.subscribe(auth => {
+        if(auth) {
+            this.router.navigateByUrl('/checkIn')
+        }
+    })
+
+  }
+
+//   onSubmit(formData) {
+//     if(formData.valid) {
+//       console.log(formData.value);
+//       this.af.auth.login({
+//         email: formData.value.email,
+//         password: formData.value.password
+//       },
+//       {
+//         provider: AuthProviders.Password,
+//         method: AuthMethods.Password,
+//       }).then(
+//         (success) => {
+//         console.log(success);
+//         this.router.navigate(['/members']);
+//       }).catch(
+//         (err) => {
+//         console.log(err);
+//         this.error = err;
+//       })
+//     }
+//   }
 
   slideToggle(){
       if (this.state == "slideIn"){
@@ -34,5 +73,9 @@ export class LoginComponent  {
           this.state = "slideIn";
       }
       console.log(this.state);
+  }
+
+  ngOnInit(){
+
   }
 }
